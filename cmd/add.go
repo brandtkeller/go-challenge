e8e058b8307e6a651a22c0e1e756f1a81d492ddd
Brandt Keller <info@brandtkeller.net>
2023-09-05 13:27:15 -0700

    Reduce function tree - improve error output

--- cmd/add.go
+++ cmd/add.go
@@ -20,24 +20,65 @@ var (
 	code bool
 )
 
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
-
 // addCmd represents the add command
 var addCmd = &cobra.Command{
 	Use:   "add",
 	Short: "Add a new challenge directory with specified name",
 	Long: `Creates a new directory with the specified name and establishes the prerequisite development
 	and testing files required to write and test a given challenge function.`,
-	Run: func(cmd *cobra.Command, args []string) {
+	RunE: func(cmd *cobra.Command, args []string) error {
 		if len(args) == 0 {
 			fmt.Println("Name is required during execution - `go-challenge add <name>`")
 			os.Exit(1)
 		}
-		run(args[0])
+
+		// Only processing one name argument...
+		name := args[0]
+
+		// Make the directory for use
+		// TODO: What permissions are best?
+		err := os.Mkdir(name, 0755)
+
+		if err != nil {
+			return fmt.Errorf("%w", err)
+		}
+
+		// Establish the version go to use via runtime
+		// This feels needlessly complex
+		goVer := runtime.Version()
+		goVer = strings.Trim(goVer, "go")
+		modVer := strings.SplitAfterN(goVer, ".", 3)
+		modVer[1] = strings.Trim(modVer[1], ".")
+		goVer = modVer[0] + modVer[1]
+
+		modFile := []byte(fmt.Sprintf("module %v\n\ngo %v\n", name, goVer))
+		err = os.WriteFile(name+"/go.mod", modFile, 0644)
+		if err != nil {
+			return fmt.Errorf("%w", err)
+		}
+
+		title := camelCase(name)
+
+		mainFile := []byte(fmt.Sprintf("package main\n\nfunc %v(){\n\n}\n\nfunc main() {\n\t%v()\n}\n", title, title))
+		err = os.WriteFile(name+"/main.go", mainFile, 0644)
+		if err != nil {
+			return fmt.Errorf("%w", err)
+		}
+
+		mainTestFile := []byte(fmt.Sprintf("package main\n\nimport \"testing\"\n\nfunc Test%v(t *testing.T) {\n\n}\n", cases.Title(language.AmericanEnglish, cases.NoLower).String(title)))
+		err = os.WriteFile(name+"/main_test.go", mainTestFile, 0644)
+		if err != nil {
+			return fmt.Errorf("%w", err)
+		}
+
+		if code {
+			cmd := exec.Command("code", "./"+name)
+			_, err = cmd.Output()
+			if err != nil {
+				return fmt.Errorf("%w", err)
+			}
+		}
+		return nil
 	},
 }
 
@@ -53,43 +94,6 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	addCmd.Flags().BoolVarP(&code, "code", "c", false, "Open vscode automatically")
-}
-
-func run(name string) {
-	// Make the directory for use
-	err := os.Mkdir(name, 0755)
-
-	// TODO: Need to gracefully exit here
-	check(err)
-
-	// Establish the version go to use via runtime
-	// This feels needlessly complex
-	goVer := runtime.Version()
-	goVer = strings.Trim(goVer, "go")
-	modVer := strings.SplitAfterN(goVer, ".", 3)
-	modVer[1] = strings.Trim(modVer[1], ".")
-	goVer = modVer[0] + modVer[1]
-
-	modFile := []byte(fmt.Sprintf("module %v\n\ngo %v\n", name, goVer))
-	err = os.WriteFile(name+"/go.mod", modFile, 0644)
-	check(err)
-
-	title := camelCase(name)
-
-	mainFile := []byte(fmt.Sprintf("package main\n\nfunc %v(){\n\n}\n\nfunc main() {\n\t%v()\n}\n", title, title))
-	err = os.WriteFile(name+"/main.go", mainFile, 0644)
-	check(err)
-
-	mainTestFile := []byte(fmt.Sprintf("package main\n\nimport \"testing\"\n\nfunc Test%v(t *testing.T) {\n\n}\n", cases.Title(language.AmericanEnglish, cases.NoLower).String(title)))
-	err = os.WriteFile(name+"/main_test.go", mainTestFile, 0644)
-	check(err)
-
-	if code {
-		cmd := exec.Command("code", "./"+name)
-		_, err = cmd.Output()
-		check(err)
-	}
-
 }
 
 // Starting point:
